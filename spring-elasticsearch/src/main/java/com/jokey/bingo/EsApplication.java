7e7678c75e59db50a8d36544d6a56b2fbc96aa1d
zhengjingfeng <zhengjingfeng@yiheni.cn>
2018-11-16 12:20:23 +0800

    添加ES模块

---
+++ spring-elasticsearch/src/main/java/com/jokey/bingo/EsApplication.java
@@ -0,0 +1,15 @@
+package com.jokey.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jokey
+ */
+@SpringBootApplication
+public class EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class, args);
+    }
+}
