fecb5ea4b54798747a249385c836509f94ba09fa
zhengjingfeng <zhengjingfeng@yiheni.cn>
2018-11-16 19:50:43 +0800

    添加websocket模块

---
+++ spring-aop-websocket/src/main/java/com/jokey/bingo/SpringAopWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.jokey.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopWebsocketApplication.class, args);
+    }
+}
